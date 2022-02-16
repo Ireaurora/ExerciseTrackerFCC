77d086cc4ad680c5932473f16c2845e218500c53
Irene Sarigu <ireaurora@gmail.com>
2022-02-16 17:47:59 +0000

    Project completed

--- server.js
+++ server.js
@@ -32,8 +32,9 @@ let User = mongoose.model('User', userSchema);
 const exerciseSchema = new Schema({
   description: String, 
   duration: Number, 
-  date: String
-}, { _id: false }); 
+  date: String,
+  userID: mongoose.Types.ObjectId
+}); 
 
 let Exercise = mongoose.model('Exercise', exerciseSchema);
 // You can POST to /api/users with form data username to create a new user.
@@ -67,20 +68,16 @@ app.get('/api/users', (req, res)=> {
 // You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
 // The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
 
-//Needs work on the date format
-
 app.post('/api/users/:_id/exercises', (req, res)=> {
    let userID =  req.params._id;
    let description = req.body.description; 
    let duration = req.body.duration; 
    let date = req.body.date; 
-
-   date = (date == '') ? new Date() : new Date(date);
+   date = (date === undefined) ? new Date() : new Date(date);
    date = date.toDateString();
-
-
    let exercise = new Exercise({
-     _id: userID, 
+     _id: new mongoose.Types.ObjectId(),
+     userID: userID, 
      description: description, 
      duration: duration, 
      date: date
@@ -92,35 +89,76 @@ app.post('/api/users/:_id/exercises', (req, res)=> {
      exercise.save((err, data)=>{
       if(err) console.log(err);
       return res.json({
-        _id: userID, 
       username: userData.username, 
       description: description, 
-      duration: duration, 
-      date: date
+      duration: Number(duration), 
+      date: date,   
+      _id: userID, 
       });
     })
    })
-
 });
 
 // You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
-
-app.get('/api/users/:_id/logs', (req, res)=>{
-  Exercise.find({}, (err, data)=>{
-      if(err) console.log(err); 
-      const number = Exercise.countDocuments();
-      // console.log('This is the count ',  number)
-      res.send(data, {count:number });
-  });
-})
-
 // A request to a user's log GET /api/users/:_id/logs returns a user object with a count property representing the number of exercises that belong to that user.
 // A GET request to /api/users/:id/logs will return the user object with a log array of all the exercises added.
 // Each item in the log array that is returned from GET /api/users/:id/logs is an object that should have a description, duration, and date properties.
+
 // The description property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.
 // The duration property of any object in the log array that is returned from GET /api/users/:id/logs should be a number.
 // The date property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.. Use the dateString format of the Date API.
 // You can add from, to and limit parameters to a GET /api/users/:_id/logs request to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. limit is an integer of how many logs to send back.
+
+app.get('/api/users/:_id/logs?', (req, res)=>{
+  let userID =  req.params._id;
+  let toParam =  req.query.to;
+  let fromParam =  req.query.from;
+  let limit = req.query.limit;
+  let username;
+  let exerciseCount;
+  let log = [];
+
+  User.findOne({_id: userID}, (err, userData)=> {
+    if(err) console.log(err);
+    username = userData.username;
+   });
+
+
+  Exercise.countDocuments({'userID': userID}, (err, count)=>{
+      if(err) console.log(err);
+      exerciseCount = count;
+      });
+
+  Exercise.find({'userID': userID }, (err, data)=>{
+      if(err) console.log(err); 
+      data.forEach(exercise => {
+        if((toParam == undefined || fromParam == undefined)
+        || 
+        ((fromParam !== undefined) && new 
+        Date(exercise.date) > new Date(fromParam))
+        ||
+        ((toParam !== undefined) && new 
+        Date(exercise.date) < new Date(toParam))
+        ) {
+        }
+        log.push({
+          'description' : exercise.description, 
+          'duration' : exercise.duration, 
+          'date' : exercise.date, 
+        });
+      });
+
+      if(limit !== undefined) { log = log.slice(0, limit);}
+
+      return res.json({
+        _id: userID, 
+      username: username, 
+      count: exerciseCount,
+      log: log
+      });
+  });
+})
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
